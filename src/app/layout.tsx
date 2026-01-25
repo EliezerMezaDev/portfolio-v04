7e2d743e8ba4677badad1c6e987677928fae4b64
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2026-01-25 19:43:11 -0400

    add: fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "@/styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "EaMZ",
+  title: "EaMZ | Frontend Developer",
   description: "Personal portfolio",
 };
 
@@ -23,10 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body className={`antialiased`} suppressHydrationWarning>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-base-main">
-      <h1 className="text-accent">Hello Home</h1>
+      <h1 className="text-accent font-poppins">Hello Home</h1>
     </div>
   );
 }
