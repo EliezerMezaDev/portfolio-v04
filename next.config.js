0ce2b2c8a3dc0a7b0177c67f8558ae8e790c43c6
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2026-05-07 23:37:50 -0400

    config: update next config and dependencies

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 module.exports = withBundleAnalyzer({
 	// your Next.js configuration
+
+	allowedDevOrigins: ["localhost", "192.168.100.138"],
+
 	images: {
 		remotePatterns: [
 			{
