f9710800aa487d296dc3fb4f88996873abceddb8
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2026-05-07 23:38:03 -0400

    refactor: update projects page structure

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,9 +6,6 @@ import Button from "@components/ui/Button";
 import Image from "next/image";
 
 // images
-import Intervyou1 from "@public/image/projects/web/intervyou/intervyou-1.png";
-import Intervyou2 from "@public/image/projects/web/intervyou/intervyou-2.png";
-import Intervyou3 from "@public/image/projects/web/intervyou/intervyou-3.png";
 import ProjectAll from "@public/image/projects.png";
 
 import Hr from "@components/ui/Hr";
@@ -19,6 +16,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import Hightlight from "@components/projects/Hightlight";
 import { CodeEffect } from "@components/ui/CodeEffect";
+import Hero from "@components/ui/Hero";
 
 const categories = [
   { id: 1, label: "Desarrollo Web" },
@@ -39,63 +37,12 @@ export default function Page() {
         <FixedButon href="/#projects">
           <FontAwesomeIcon icon={faChevronLeft} className="text-black pr-10" />
         </FixedButon>
-        <div className="relative h-screen w-screen  gap-4 p-10 flex justify-center items-center flex-col mb-10 overflow-hidden">
-          <div className="z-0 mb-48 md:mb-0  md:absolute top-1/4  md:right-[10%] md:-translate-y-16 ">
-            <motion.div
-              initial={{ scale: 1 }}
-              animate={{ scale: 1.6 }}
-              transition={{ duration: 1, ease: "circOut" }}
-              className="relative bg-slate-300 rounded-sm h-[400px] md:h-[600px] w-[80vw] md:w-[30vw] grayscale hover:grayscale-0 "
-            >
-              <Image
-                src={ProjectAll}
-                alt="EaMZ"
-                fill
-                placeholder="blur"
-                className="object-cover"
-                sizes="(max-width: 768px) 80vw, 30vw"
-              />
-            </motion.div>
-          </div>
-          <div className="z-10 w-full absolute md:w-auto md:left-[10%] top-[60%] md:top-1/3 col-span-2 flex flex-col justify-center items-start md:items-start text-start px-10 pt-4 backdrop-filter backdrop-blur-sm md:backdrop-blur-none md:backdrop-filter-none bg-gray-100 bg-opacity-50 md:bg-transparent md:pt-0">
-            <CodeEffect
-              className="mb-2 md:mb-5 text-5xl md:text-7xl"
-              symbolClassName="text-black/10"
-              children={
-                <motion.h1
-                  className="bg-white lg:bg-transparent bg-opacity-50 text-main font-bold"
-                  initial={{ x: -100, opacity: 0 }}
-                  whileInView={{ x: 0, opacity: 1 }}
-                  transition={{
-                    delay: 0.1,
-                    type: "spring",
-                  }}
-                >
-                  MisProyectos
-                </motion.h1>
-              }
-            />
-            <Hr />
-            <p className="title  text-xl mt-4 tracking-wider text-gray-900 leading-[1.7rem] mb-5">
-              Algunos proyectos que he creado a lo largo de los años <br /> y en
-              los que estoy trabajando actualmente.
-            </p>
-            <motion.div
-              initial={{ opacity: 0, y: 100 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, ease: "circOut" }}
-              onClick={() => {
-                window.scrollTo({
-                  top: 1000,
-                  behavior: "smooth",
-                });
-              }}
-              className="mb-3"
-            >
-              <Button variation="primary">Scroll Down</Button>
-            </motion.div>
-          </div>
-        </div>
+
+        <Hero
+          title="Mis proyectos"
+          desc="Aquí encontraras algunos de los proyectos en los que he trabajado"
+          coverImg={ProjectAll}
+        />
 
         <Hightlight />
 
